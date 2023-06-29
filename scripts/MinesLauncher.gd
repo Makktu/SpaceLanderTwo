61b9c0c2e9b9f6a8ba202605bbf879dad5381070
Makktu <johnmcna@gmail.com>
2023-06-29 13:40:11 +0100

    Pathfinding and spawn behaviour

--- scripts/MinesLauncher.gd
+++ scripts/MinesLauncher.gd
@@ -3,20 +3,26 @@ extends Sprite
 
 onready var basic_enemy = preload("res://scenes/Enemy1.tscn")
 var already_triggered = false
+var amount_spawned = 0
 
 
 func spawn_mine():
 	var loaded_enemy = basic_enemy.instance()
 	get_node("spawn_node").add_child(loaded_enemy)
-	$activation_animation.stop()
+
 	
 
 func _on_Area2D_body_entered(body):
 	if body.name == 'player' and !already_triggered:
+		already_triggered = true
 		$activation_animation.play("activation")
 		$SpawnTimer.start()
 
 
 func _on_SpawnTimer_timeout():
-	already_triggered = true
-	spawn_mine()
+	amount_spawned += 1
+	if amount_spawned < 4:
+		spawn_mine()
+		$SpawnTimer.start()
+	else:
+		$activation_animation.stop()
--- scripts/StartScreen.gd
+++ scripts/StartScreen.gd
@@ -1,12 +1,11 @@
-extends Control
-
+extends CenterContainer
 
 func _ready():
-	$SpaceLander2_title.visible = true
-	$StartButton.visible = true
+#	$SpaceLander2_title.visible = true
+#	$StartButton.visible = true
 	$AnimationPlayer.play("fade_in")
-	$StartButtonAnimation.play("btn_fade_in")
-
+	$"../CenterContainer/StartButtonAnimation".play("btn_fade_in")
 
 func _on_StartButton_pressed():
 	print("START PRESSED")
+	get_tree().change_scene("res://World.tscn")
