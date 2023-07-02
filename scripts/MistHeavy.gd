dc5195555f6422f5392859018233356e75cba3ed
Makktu <johnmcna@gmail.com>
2023-07-02 21:44:09 +0100

    mist finalised

--- scripts/MistHeavy.gd
+++ scripts/MistHeavy.gd
@@ -7,12 +7,12 @@ var update_pos = 0
 func _physics_process(_delta):
 	position.x += movement_direction
 	# may have to spawn these in and control them that way!
-#	if position.x > -1000:
-#		movement_direction = -0.5
-#		position.x = -1000
-#	if position.x < -4600:
-#		movement_direction = 0.5
-#		position.x = -4600
+	if position.x > 15790:
+		movement_direction = -0.5
+		position.x = 15790
+	if position.x < -14940:
+		movement_direction = 0.5
+		position.x = -14935
 	update_pos += 1
 	if update_pos > 100:
 		print('MIST_HEAVY1:', position.x)
