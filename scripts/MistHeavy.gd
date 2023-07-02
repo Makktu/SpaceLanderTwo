ab63a258c3a49c0056f0e81b275ea0e87e60a50c
Makktu <johnmcna@gmail.com>
2023-07-02 21:10:26 +0100

    better mist

--- scripts/MistHeavy.gd
+++ scripts/MistHeavy.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 var movement_direction = 0.5
 
+var update_pos = 0
+
 func _physics_process(_delta):
 	position.x += movement_direction
-	if position.x > -1000:
-		movement_direction = -0.5
-		position.x = -1000
-	if position.x < -4600:
-		movement_direction = 0.5
-		position.x = -4600
+	# may have to spawn these in and control them that way!
+#	if position.x > -1000:
+#		movement_direction = -0.5
+#		position.x = -1000
+#	if position.x < -4600:
+#		movement_direction = 0.5
+#		position.x = -4600
+	update_pos += 1
+	if update_pos > 100:
+		print('MIST_HEAVY1:', position.x)
+		update_pos = 0
 	
