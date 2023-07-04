261679fe2f5492f9db2e49e1be9a1abb1414927b
Makktu <johnmcna@gmail.com>
2023-07-04 20:38:55 +0100

    tweaking this and that

--- scripts/MinesLauncher.gd
+++ scripts/MinesLauncher.gd
@@ -4,6 +4,7 @@ extends Sprite
 onready var basic_enemy = preload("res://scenes/Enemy1.tscn")
 var already_triggered = false
 var amount_spawned = 0
+var amount_to_be_spawned = 4
 
 
 func spawn_mine():
@@ -15,17 +16,24 @@ func spawn_mine():
 func _on_Area2D_body_entered(body):
 	if body.name == 'player' and !already_triggered:
 		already_triggered = true
-#		$activation_animation.play("activation")
-		$Particles2D.emitting = true
+		$activation_animation.play("activation")
 		$SpawnTimer.start()
 
 
 func _on_SpawnTimer_timeout():
-
 	amount_spawned += 1
-	if amount_spawned < 4:
+	if amount_spawned < amount_to_be_spawned:
 		spawn_mine()
 		$SpawnTimer.start()
 	else:
-#		$activation_animation.stop()
-		$Particles2D.emitting = false
+		$activation_animation.stop()
+		$DormancyTimer.start()
+
+
+func _on_DormancyTimer_timeout():
+	# resets the spawner so it will trigger again if the player passes by
+	already_triggered = false
+	amount_spawned = 0
+	amount_to_be_spawned += 2
+	if amount_to_be_spawned > 10:
+		amount_to_be_spawned = 10
