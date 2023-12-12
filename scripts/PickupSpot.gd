260d4ac176a3cce645763769cc7da4e9921792aa
Makktu <johnmcna@gmail.com>
2023-12-12 21:11:01 +0000

    pickups again

--- scripts/PickupSpot.gd
+++ scripts/PickupSpot.gd
@@ -11,12 +11,15 @@ func _ready() -> void:
 	
 	
 func deploy_pickup():
+	if $"/root/Global".player_hits >= 8:
+		shield_chance = 80
+	if shield_chance > 10 and $"/root/Global".player_hits < 8:
+		shield_chance = 10
 	speedup = false
 	shield_on = false
 	lightning_on = false
 	
-	var pickup_chance = $"/root/Global".random_float_number(1, 50)
-	
+	var pickup_chance = $"/root/Global".random_float_number(1, 50)	
 	if pickup_chance < 20:
 		speedup = true
 	else:
@@ -26,28 +29,21 @@ func deploy_pickup():
 			shield_on = true
 		else:
 			lightning_on = true
-		
-		if shield_chance != 10:
-			shield_chance = 10
 	
 	show_pickup()
 	
 func show_pickup():
 	if shield_on:
 		$Shield.visible = true
-	elif lightning_on:
+	if lightning_on:
 		$Sprite.visible = true
-	elif speedup:
+	if speedup:
 		$Speedup.visible = true
-	else:
-		print("MAJOR BUG HERE")		
-		
+		$Speedup.playing = true
+	$ChangeTimer.start()
 	
 func _on_PickupSpot_body_entered(body):
-	if body.name == 'player' and visible:
-		var spawn_time_now = $"/root/Global".random_float_number(5, 10)
-		$SpawnTimer.wait_time = spawn_time_now
-		$SpawnTimer.start()
+	if body.name == 'player':
 		$AnimationPlayer.play("fade")
 		if shield_on:
 			$"/root/Global".handle_pickups('shield')
@@ -61,19 +57,8 @@ func _on_PickupSpot_body_entered(body):
 			$"/root/Global".handle_pickups("speedup")
 
 
-func _on_SpawnTimer_timeout():
-	visible = true
-	deploy_pickup()
-
-
 func _on_AnimationPlayer_animation_finished(anim_name):
-	lightning_on = false
-	speedup = false
-	shield_on = false
-	visible = false
-	$Sprite.visible = false
-	$Shield.visible = false
-	$Speedup.visible = false
+	queue_free()
 
 
 func _on_VisibilityNotifier2D_screen_entered() -> void:
@@ -81,6 +66,13 @@ func _on_VisibilityNotifier2D_screen_entered() -> void:
 	if speedup:
 		$Speedup.visible = true
 		$Speedup.playing = true
+	if $"/root/Global".player_hits >= 8:
+		speedup = false
+		lightning_on = false
+		shield_on = true
+		$Shield.visible = true
+		$Sprite.visible = false
+		$Speedup.visible = false
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	$Particles2D.emitting = false
@@ -88,5 +80,16 @@ func _on_VisibilityNotifier2D_screen_exited() -> void:
 	$Speedup.visible = true
 
 
-func _on_DelayTimer_timeout() -> void:
-	$"/root/Global".pause_or_game_over("first_pickup")
+func _on_ChangeTimer_timeout():
+	speedup = false
+	lightning_on = false
+	shield_on = false
+	$Shield.visible = false
+	$Sprite.visible = false
+	$Speedup.visible = false
+	$Speedup.playing = false
+	$SpawnTimer.start()
+
+
+func _on_SpawnTimer_timeout():
+	deploy_pickup()
